4520e4e5e6301bf911e20d75ed1008dd5aca9e2d
pravesh1731 <157206714+pravesh1731@users.noreply.github.com>
2025-07-18 23:07:29 +0530

    Update auth_service.dart

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Sign up with email and password
+  
   Future<User?> signUp(String email, String password) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
@@ -17,7 +17,7 @@ class AuthService {
     }
   }
 
-  // Login with email and password
+  
   Future<User?> signIn(String email, String password) async {
     try {
       UserCredential result = await _auth.signInWithEmailAndPassword(
@@ -31,12 +31,12 @@ class AuthService {
     }
   }
 
-  // Sign out
+
   Future<void> signOut() async {
     await _auth.signOut();
   }
 
-  // Get current user
+  
   User? getCurrentUser() {
     return _auth.currentUser;
   }

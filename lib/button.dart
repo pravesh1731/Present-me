b29564eedadee295644d20166ac97c0cb16a96a7
pravesh1731 <706890pk@gmail.com>
2025-08-04 13:19:24 +0530

    state managemaent of student list at mark attendance page

--- lib/button.dart
+++ lib/button.dart
@@ -68,7 +68,7 @@ class Logo extends StatelessWidget {
           decoration: BoxDecoration(
             shape: BoxShape.circle,
             gradient: LinearGradient(
-              colors: [Color(0xff0BCCEB), Color(0xff0A80F5)], // Customize colors
+              colors: [Color(0xff0BCCEB), Color(0xff0A80F5)],
               begin: Alignment.bottomLeft,
               end: Alignment.topCenter,
             ),
@@ -79,7 +79,7 @@ class Logo extends StatelessWidget {
         SizedBox(
           width: 60,
           height: 60,
-          child: Image.asset("assets/image/logo.png"), // Replace with your avatar image
+          child: Image.asset("assets/image/logo.png"),
         ),
       ],
     );
